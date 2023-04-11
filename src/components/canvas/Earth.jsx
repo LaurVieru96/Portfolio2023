30aad1164aa31573700d06c8df714b5268b51814
Laur <laurentiuvieru50@gmail.com>
2023-04-11 14:55:03 +0300

    fix

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -21,6 +21,9 @@ const EarthCanvas = () => {
       gl={{ preserveDrawingBuffer: true }}
       camera={{
         fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [-4, 3, 6],
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,7 +5,9 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -17,7 +19,7 @@ const Stars = (props) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color='#f272c8'
+          color="#f272c8"
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
@@ -29,7 +31,7 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className='w-full h-auto absolute inset-0 z-[-1]'>
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
@@ -42,3 +44,7 @@ const StarsCanvas = () => {
 };
 
 export default StarsCanvas;
+
+// 2:32:09 / 2:53:18
+
+// https://www.youtube.com/watch?v=0fYi8SGA20k&ab_channel=JavaScriptMastery
